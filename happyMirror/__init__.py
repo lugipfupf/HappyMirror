b9c25742713fe18645b0e1dfa081d468537e8124
Beat Luginbühl <lugi@lugipfupf.ch>
2024-04-15 23:44:48 +0200

    dynamically load widgets

--- happyMirror/__init__.py
+++ happyMirror/__init__.py
@@ -1,17 +1,12 @@
-"""
-dasdsa
-"""
-
 import os
+import importlib
+import sys
 
 from flask import Flask, render_template
-from happyMirror.quote import Quote
+import config
 
 
 def create_app(test_config=None):
-    """
-    dsadsa
-    """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping()
 
@@ -25,16 +20,40 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    quote  = Quote()
-
+    # quote  = Quote()
 
     @app.route('/')
     def root():
-        return render_template('base.html', quote=quote.next())
-
+        return 'blah'
+        # return render_template('base.html', quote=quote.next())
 
     return app
 
 
+def load_widgets(enabled_widgets=None):
+    if enabled_widgets is None:
+        enabled_widgets = []
+
+    for root, dirs, files in walklevel(os.curdir + '/widgets'):
+        for dir_name in dirs:
+            if dir_name in enabled_widgets:
+                sys.path.append(root)
+                print(f"Importing widget '{dir_name}'")
+                widget_package = importlib.import_module(dir_name)
+                print(f"Widget '{widget_package.__name__}' loaded")
+
+
+def walklevel(some_dir, level=1):
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
+
+
 if __name__ == '__main__':
-    create_app().run()
+    load_widgets(config.enabled_widgets)
+    # create_app().run()
--- happyMirror/widgets/quote/quote.py
+++ happyMirror/widgets/quote/quote.py
@@ -2,20 +2,24 @@ import random
 import requests
 import json
 
-from . import quote_categories
+from .quote_categories import categories
+from .widget_secrets import api_key
 
 
+class Quote:
+    def __init__(self, secrets_config=None) -> None:
+        if secrets_config is None:
+            raise "No secrets provided"
 
-class Quote():
-    def __init__(self) -> None:
         self.url = 'https://api.api-ninjas.com/v1/quotes?category='
+        print("Hello Quote")
 
 
     def next(self):
-        category = random.choice(quote_categories.categories)
+        category = random.choice(categories)
 
         quote = requests.get(
-            self.url + category, headers={'X-Api-Key': 'your-api-key'}, timeout=10
+            self.url + category, headers={'X-Api-Key': api_key}, timeout=10
         )
 
         return json.loads(quote.text)
